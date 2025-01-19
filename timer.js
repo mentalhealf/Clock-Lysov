1db8b87787d5b062835783715a59ffa3160af0ba
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-01-19 11:48:05 +0000

    added ringtone to timer

--- timer.js
+++ timer.js
@@ -16,7 +16,9 @@ const minutesInput = document.getElementById("timer-minutes");
 const startButton = document.getElementById("start-timer");
 const stopButton = document.getElementById("stop-timer");
 
-startButton.addEventListener("click", () => {
+const ringtone = new Audio("./Ringtong/ringtong_August.mp3");
+
+  startButton.addEventListener("click", () => {
     if (timerInterval) return;
     const minutes = parseInt(minutesInput.value);
     if (isNaN(minutes) || minutes <= 0) {
@@ -30,26 +32,68 @@ startButton.addEventListener("click", () => {
         timerInterval = null;
         display.textContent = "Time is up!";
         display.classList.add("animate-time-up"); 
+
+   
+        ringtone.play();
       } else {
         remainingTime -= 1000;
         display.textContent = formatTime(remainingTime);
       }
     }, 1000);
   });
+
   stopButton.addEventListener("click", () => {
     clearInterval(timerInterval);
     timerInterval = null;
     display.textContent = "00:00";
-    display.classList.remove("animate-time-up"); // При зупинці прибираємо анімацію
+    display.classList.remove("animate-time-up");  анімацію
   });
+
   function formatTime(ms) {
     const totalSeconds = Math.floor(ms / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
     return `${pad(minutes)}:${pad(seconds)}`;
   }
+
   function pad(number) {
     return number.toString().padStart(2, "0");
   }
 }
+// startButton.addEventListener("click", () => {
+//     if (timerInterval) return;
+//     const minutes = parseInt(minutesInput.value);
+//     if (isNaN(minutes) || minutes <= 0) {
+//       display.textContent = "Invalid input";
+//       return;
+//     }
+//     let remainingTime = minutes * 60 * 1000;
+//     timerInterval = setInterval(() => {
+//       if (remainingTime <= 0) {
+//         clearInterval(timerInterval);
+//         timerInterval = null;
+//         display.textContent = "Time is up!";
+//         display.classList.add("animate-time-up"); 
+//       } else {
+//         remainingTime -= 1000;
+//         display.textContent = formatTime(remainingTime);
+//       }
+//     }, 1000);
+//   });
+//   stopButton.addEventListener("click", () => {
+//     clearInterval(timerInterval);
+//     timerInterval = null;
+//     display.textContent = "00:00";
+//     display.classList.remove("animate-time-up"); // При зупинці прибираємо анімацію
+//   });
+//   function formatTime(ms) {
+//     const totalSeconds = Math.floor(ms / 1000);
+//     const minutes = Math.floor(totalSeconds / 60);
+//     const seconds = totalSeconds % 60;
+//     return `${pad(minutes)}:${pad(seconds)}`;
+//   }
+//   function pad(number) {
+//     return number.toString().padStart(2, "0");
+//   }
+// }
 
