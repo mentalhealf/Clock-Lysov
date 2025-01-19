349b359407f6e93569f5b9ba76800f294a3da075
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-01-19 11:07:24 +0000

    added ringtong

--- alarm.js
+++ alarm.js
@@ -9,28 +9,59 @@ export function initAlarm() {
 `;
 
 const setAlarmButton = document.getElementById("set-alarm");
-setAlarmButton.addEventListener("click", () => {
+const alarmMessage = document.getElementById("alarm-message");
+
+  const ringtone = new Audio("./Ringtong/ringtong_August.mp3");
+
+  setAlarmButton.addEventListener("click", () => {
     const alarmTime = document.getElementById("alarm-time").value;
-    const alarmMessage = document.getElementById("alarm-message");
+
     if (!alarmTime) {
-        alarmMessage.textContent = "Please set a valid time.";
-        return;
+      alarmMessage.textContent = "Please set a valid time.";
+      return;
+    }
+
+    setAlarmButton.style.backgroundColor = "green";
+    setAlarmButton.textContent = "Alarm Set!";
+    alarmMessage.textContent = "";
+
+    const alarm = new Date();
+    const [hours, minutes] = alarmTime.split(":");
+    alarm.setHours(hours, minutes, 0);
+
+    const checkAlarm = setInterval(() => {
+      const now = new Date();
+      if (now >= alarm) {
+        clearInterval(checkAlarm);
+        alarmMessage.textContent = "Wake up!";
+        alarmMessage.classList.add("animate-wake-up");
+        ringtone.play();
       }
-      setAlarmButton.style.backgroundColor = "green";
-      setAlarmButton.textContent = "Alarm Set!";
-      const alarm = new Date();
-      const [hours, minutes] = alarmTime.split(":");
-      alarm.setHours(hours, minutes, 0);
-      const checkAlarm = setInterval(() => {
-        const now = new Date();
-        if (now >= alarm) {
-          alarmMessage.textContent = "Wake up!";
-          alarmMessage.classList.add("animate-wake-up"); 
-          clearInterval(checkAlarm);
-        }
-      }, 1000);
-})
+    }, 1000);
+  });
 }
+// setAlarmButton.addEventListener("click", () => {
+//     const alarmTime = document.getElementById("alarm-time").value;
+//     const alarmMessage = document.getElementById("alarm-message");
+//     if (!alarmTime) {
+//         alarmMessage.textContent = "Please set a valid time.";
+//         return;
+//       }
+//       setAlarmButton.style.backgroundColor = "green";
+//       setAlarmButton.textContent = "Alarm Set!";
+//       const alarm = new Date();
+//       const [hours, minutes] = alarmTime.split(":");
+//       alarm.setHours(hours, minutes, 0);
+//       const checkAlarm = setInterval(() => {
+//         const now = new Date();
+//         if (now >= alarm) {
+//           alarmMessage.textContent = "Wake up!";
+//           alarmMessage.classList.add("animate-wake-up"); 
+//           clearInterval(checkAlarm);
+//         }
+//       }, 1000);
+// })
+// }
 
 
 
